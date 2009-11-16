7da96a39f11dcd566cb45c4cc7a560b9ef44e22b
Magnus Lidbom <magnus.lidbom@gmail.com>
2009-11-16 18:48:50 +0100

    Add a monteCarlo algorithm.

--- AbelsonSussman/Algorithms.fsx
+++ AbelsonSussman/Algorithms.fsx
@@ -1,5 +1,6 @@
 ﻿
 module Algorithms
+
 let fixedPoint nextGuess goodEnough initialGuess =         
     let rec tryGuess lastGuess guess =  
         if goodEnough lastGuess guess
@@ -7,3 +8,11 @@ let fixedPoint nextGuess goodEnough initialGuess =
         else tryGuess guess (nextGuess guess)
     
     tryGuess initialGuess (nextGuess initialGuess)
+
+let monteCarlo trials test =
+    let rec doTest passed remaining = 
+        match remaining with
+        | 0 -> float passed / float trials
+        | n -> doTest (if test() then passed + 1 else passed) (remaining - 1)
+
+    doTest 0 trials        
