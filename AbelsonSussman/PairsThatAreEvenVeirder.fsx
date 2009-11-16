396e46f5eb2129890aa2877e54736fb3de2b9a60
Magnus Lidbom <magnus.lidbom@gmail.com>
2009-11-16 18:48:35 +0100

    Even odder pairs.

---
+++ AbelsonSussman/PairsThatAreEvenVeirder.fsx
@@ -0,0 +1,11 @@
+﻿
+
+
+let MakePair =  fun first second -> fun selector -> selector first second
+let First  = fun pair -> pair (fun first second -> first)
+let Second = fun pair -> pair (fun first second -> second)
+
+let oneAndTwo = MakePair 1 2
+let one = First oneAndTwo
+let two = Second oneAndTwo
+
