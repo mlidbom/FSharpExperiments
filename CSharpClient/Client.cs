7fa662976acd11cd54c61c8284bbdb296957ef18
malm <magnus.lidbom@gmail.com>
2010-10-12 01:10:07 +0200

    Playing around with eposing F# code to C#

---
+++ CSharpClient/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using Exts;
+using System.Linq;
+
+namespace CSharpClient
+{
+    [TestFixture]
+    public class Client
+    {
+        [Test]
+        public void TestString()
+        {
+            Assert.That("".IsNullOrWhiteSpace(), Is.True);
+        }
+    }
+}
---
+++ FSharpLib/StringExtensions.fs
@@ -0,0 +1,24 @@
+﻿[<System.Runtime.CompilerServices.Extension>]    
+module Exts.CollectionExtensions
+  //Extension method
+    [<System.Runtime.CompilerServices.Extension>]    
+    let IsNullOrWhiteSpace this = System.String.IsNullOrWhiteSpace this
+    
+    //Static extension method
+    [<System.Runtime.CompilerServices.Extension>]    
+    let MyLength (aString:string) = aString.Length
+
+
+
+
+
+//[<System.Runtime.CompilerServices.Extension>]    
+//module Extensions.String
+//
+//[<System.Runtime.CompilerServices.Extension>]    
+//let IsNullOrWhiteSpace this = System.String.IsNullOrWhiteSpace this
+//
+//
+//
+//
+//
